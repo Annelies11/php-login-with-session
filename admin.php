fd33151c5d37e69f3ad9434a8d59e8342d88504c
Annelies11 <mmahmudi1103@gmail.com>
2024-04-03 22:33:26 +0700

    fitur update

--- admin.php
+++ admin.php
@@ -26,6 +26,8 @@
       .container .admin {
         color: white;
       }
+      
+      
     </style>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   </head>
@@ -37,14 +39,14 @@
         <br>
         <a href="logout.php">Logout</a>
         <br>
-        <span>
-                        <?php
-                        if(isset($_SESSION['message'])){
-                            echo $_SESSION['message'];
-                        }
-                        unset($_SESSION['message']);
-                        ?>
-                    </span>
+        <span>  
+          <?php
+          if(isset($_SESSION['message'])){
+              echo $_SESSION['message'];
+          }
+          unset($_SESSION['message']);
+          ?>
+        </span>
       </div>
       <div class="row mt-2">
         <div class="col">
@@ -75,26 +77,90 @@
                   <td><?php echo $row['name']; ?></td>
                   <td><?php echo $row['address']; ?></td>
                   <td><?php echo $row['email']; ?></td>
-                  <td><a href="update.php?updateId=<?php echo $row['id'];?>" class="btn btn-primary" style="margin-right:10px;"><span class="material-symbols-outlined">edit</span></a>
-                  <!-- <a href="delete.php?deleteId=<?php echo $row['id'];?>" class="btn btn-danger"><span class="material-symbols-outlined">delete</span></a></td> -->
+                  
+                  <td><a href="#" class="btn btn-primary" style="margin-right:10px;" data-toggle="modal" data-target="#editModal<?php echo $row['id'];?>"><span class="material-symbols-outlined">edit</span></a>  
                   <a onclick="deleteData(<?php echo $row['id']; ?>)" class="btn btn-danger"><span class="material-symbols-outlined">delete</span></a></td>
-                  </tr>
+                  <!--modalEdit start-->
+                  <form action="" method="POST">
+                  <div class="modal fade" id="editModal<?php echo $row['id'];?>" tabindex="-1" role="dialog" aria-labelledby="editModalTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                        <div  class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLongTitle">Update Data ID : <?php echo $row['id'];?></h5>
+                        </div>
+                        <div class="modal-body">
+                        <h4 class="form-heading">Name :</h4>
+                        <input type="Text" value="<?php echo $row['name']; ?>" name="name" class="form-control" id="" >
+                        <h4 class="form-heading">Address :</h4>
+                        <input type="Text" value="<?php echo $row['address']; ?>" name="address" class="form-control" id="">
+                        <h4 class="form-heading">Email :</h4>
+                        <input type="Text" value="<?php echo $row['email']; ?>" name="email" class="form-control" id="">
+                        <input type="hidden" value="<?php echo $row['id']; ?>" name="id" class="form-control" id="">
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <button type="submit" class="btn btn-primary" name="update">Update</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </form>
+                  <?php
+                    if(isset($_POST['update'])){
+                      $id = $_POST['id'];
+                      $name = $_POST['name'];
+                      $address = $_POST['address'];
+                      $email = $_POST['email'];
+
+                      $sql = "UPDATE sample SET name='$name', address='$address', email='$email' WHERE id=$id";
+                      $res = mysqli_query($conn, $sql);
+                      if(!$res){
+                        trigger_error("Error bro :".mysqli_error($res));
+                      } else {
+                        $_SESSION['message']="Update success!";
+                        echo "<meta http-equiv='refresh' content=0.1; url=?page=admin>";
+                      }
+                    }
+                  ?>
+                  <!--modalEdit end-->
+                </tr>
+                  
 
                   <?php
                   }
                   ?>
-                <script>
-                  function deleteData(id) {
-                    if(window.confirm("hapus?")) {
-                      window.location.href = 'delete.php?deleteId='+id;
-                    }
-                  }
-                  </script>
+                  
+                
               </table>
+              
+              
             </div>
           </div>
         </div>
       </div>
+      <br>
+                <!-- Button trigger modal -->
+      <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">
+        Launch demo modal
+      </button> -->
     </div>
+    <br>
+
+
+<!-- Modal -->
+
+    <script>
+      function updateData(id){
+        document.getElementById("demo").innerHTML = "I have changed!";  
+      }
+      function deleteData(id) {
+        if(window.confirm("hapus?")) {
+          window.location.href = 'delete.php?deleteId='+id;
+        }
+      }
+    </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
--- update.php
+++ update.php
@@ -1,11 +1,2 @@
 <?php
 
-echo "<script>Swal.fire({
-    title: 'Apakah anda ingin menghapus?',
-    icon: 'question',
-    iconHtml: '?',
-    confirmButtonText: 'Iya',
-    cancelButtonText: 'Tidak',
-    showCancelButton: true,
-    showCloseButton: true
-  });</script>";
